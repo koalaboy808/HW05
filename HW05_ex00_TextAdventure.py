cfa36449c2da04baa66cf6b60e5831135cf99338
koalaboy808 <carlo_liquido@ischool.berkeley.edu>
2015-08-11 17:47:14 -0700

    changes to TextAdventure 8/11/15

--- HW05_ex00_TextAdventure.py
+++ HW05_ex00_TextAdventure.py
@@ -2,7 +2,7 @@
 # HW05_ex00_TextAdventure.py
 ##############################################################################
 # Imports
-from sys import exit
+
 import sys
 # Body
 
@@ -14,31 +14,33 @@ def infinite_stairway_room(username, count=0):
     
     # infinite stairs option
     if next == "take stairs":
-        print username + 'take the stairs'
+        print username + ' take the stairs'
         print count
         if (count > 0):
             print "but you're not happy about it"
         infinite_stairway_room(username, count + 1)
     # option 2 == ?????
-    if next == "take magic mushroom":
-        cthulhu_room(username)
+    else:
+        why = "%s, you suddently start to trip way too hard and die" % username
+        dead(why, username)
 
 
 def gold_room(username):
     print "This room is full of gold.  How much do %s take?" % username
 
     next = raw_input("> ")
-    if "0" in next or "1" in next:
+    try: 
         how_much = int(next)
-    else:
-        why = "Man, learn to type a number."
+    except:
+        why = "%s you're not the dudeness, learn to type a number." % username
         dead(why, username)
-
-    if how_much < 50:
-        print "Nice, you're not greedy, you win!"
-        exit(0)
     else:
-        dead("%s, you greedy bastard!") % username
+        if how_much < 50:
+            print "Nice, you're not greedy, you win!"
+            exit(0)
+        else:
+            why = "%s, you greedy bastard" % username
+            dead(why, username)
 
 
 def bear_room(username):
@@ -53,16 +55,14 @@ def bear_room(username):
 
         if next == "take" or next == "honey":
             why = "The bear looks at %s then slaps your face off... now you're in hell" % username
-            print(why)
-            #dead(why, username)
-            infinite_stairway_room(username, count=0)
-
+            dead(why, username)
         elif next == "taunt" and not bear_moved:
             print "The bear has moved from the door. %s can go through it now." % username
             bear_moved = True
         elif next == "taunt" and bear_moved:
-            dead("The bear gets pissed off and chews your leg off.")
-        elif next == "open" or next == "door" and bear_moved:
+            why = "The bear gets pissed off and chews %s leg off." % username
+            dead(why, username)
+        elif (next == "open" or next == "door") and bear_moved:
             gold_room(username)
         else:
             print "I got no idea what that means."
@@ -78,7 +78,8 @@ def cthulhu_room(username):
     if "flee" in next:
         main()
     elif "head" in next:
-        dead("Well that was tasty!")
+        why = "Well that was tasty! %s, you taste like kale..." % username
+        dead(why, username)
     else:
         cthulhu_room(username)
 
@@ -87,27 +88,48 @@ def dead(why, username):
     print why, "Good job!"
     exit(0)
 
+def back_room(username):
+    print "So many awkward programmers! says little person in my head"
+    print "I am %s! here me roar" % username
+
+    while True:
+        
+        # keeps repeating
+        user_input = raw_input("You shall never leave now!\n")
+        
+        # break with supuku
+        if user_input=="supuku":
+            break
+
 
 ##############################################################################
 def main():
 
     # START the TextAdventure game
-    username = sys.argv[1]
-    print username + "... what a horrible name"
-
-    print username + " is in a dark room."
-    print "There is a door to your right and left."
-    print "Which one does %s take?" % username
-
-    next = raw_input("> ")
-
-    if next == "left":
-        bear_room(username)
-    elif next == "right":
-        cthulhu_room(username)
+    try:
+        username = sys.argv[1]
+    except:
+        print("Give me your name dumb dumb")
     else:
-        why = "%s stumble around the room until you starve." % username
-        dead(why, username)
+        print username + "... what a horrible name"
+
+        print username + " is in a dark room."
+        print "There is a door to your right and left, and a secret one in the back"
+        print "Which one does %s take?" % username
+
+        next = raw_input("> ")
+
+        if next == "left":
+            bear_room(username)
+        elif next == "right":
+            cthulhu_room(username)
+        elif next == "back":
+            back_room(username)
+        elif next == "meditate":
+            infinite_stairway_room(username)
+        else:
+            why = "%s stumble around the room until you starve." % username
+            dead(why, username)
 
 if __name__ == '__main__':
     main()
